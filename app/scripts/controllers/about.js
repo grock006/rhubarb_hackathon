6601119a3eb8a1495cc7d7f3233f5f3e4108af20
Greg Rock <grock006@gmail.com>
2015-09-17 14:58:39 -0700

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nomnomnomApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nomnomnomApp
+ */
+angular.module('nomnomnomApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
